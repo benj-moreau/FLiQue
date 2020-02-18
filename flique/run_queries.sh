f7f3d37793a9ded14d59db1dcdff73087b071a43
Folkvir <folkvir@outlook.fr>
2020-02-18 10:57:19 +0100

    add run_queries script

---
+++ flique/run_queries.sh
@@ -0,0 +1,79 @@
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S1 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S2 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S3 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S4 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S5 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S6 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S7 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S8 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S9 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S10 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S11 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S12 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S13 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S14 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 C1 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 C2 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 C3 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 C4 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 C5 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 C6 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 C7 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 C8 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 C9 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 C10 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 L1 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 L2 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 L3 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 L4 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 L5 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 L6 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 L7 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 L8 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 CH1 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 CH2 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 CH3 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 CH4 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 CH5 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 CH6 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 CH7 &
+wait
+timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 CH8 &
