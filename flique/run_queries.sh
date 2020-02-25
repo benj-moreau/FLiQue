12bef2332218263db5c0e7f9d52dc93eed9c836d
Folkvir <folkvir@outlook.fr>
2020-02-25 23:12:33 +0100

    add cache.db supression in exp

--- flique/run_queries.sh
+++ flique/run_queries.sh
@@ -1,79 +1,119 @@
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S1 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S2 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S3 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S4 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S5 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S6 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S7 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S8 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S9 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S10 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S11 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S12 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S13 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 S14 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 C1 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 C2 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 C3 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 C4 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 C5 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 C6 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 C7 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 C8 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 C9 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 C10 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 L1 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 L2 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 L3 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 L4 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 L5 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 L6 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 L7 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 L8 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 CH1 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 CH2 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 CH3 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 CH4 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 CH5 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 CH6 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 CH7 &
 wait
+rm cache.db
 timeout --signal=SIGKILL $1 java -jar target/flique-0.0.1-SNAPSHOT.jar $2 $3 CH8 &
