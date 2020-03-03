cbf06c58dbe6710677b1e66ff141f0ebe05de03b
benjimor <benji_moreau@hotmail.fr>
2020-03-03 11:17:22 +0100

    change execution order of experiences, change costfed parameters

--- flique/run_flique_exp.sh
+++ flique/run_flique_exp.sh
@@ -3,10 +3,6 @@ for i in `seq 1 $2`;
 do
         sh ./run_queries.sh $1 FLIQUE noRelax &
         wait
-done
-wait
-for i in `seq 1 $2`;
-do
         sh ./run_queries.sh $1 FLIQUE relax &
         wait
 done
--- flique/run_strategy_exp.sh
+++ flique/run_strategy_exp.sh
@@ -3,22 +3,10 @@ for i in `seq 1 $2`;
 do
         sh ./run_queries.sh $1 BFS relax &
         wait
-done
-wait
-for i in `seq 1 $2`;
-do
         sh ./run_queries.sh $1 OBFS relax &
         wait
-done
-wait
-for i in `seq 1 $2`;
-do
         sh ./run_queries.sh $1 OMBS relax &
         wait
-done
-wait
-for i in `seq 1 $2`;
-do
         sh ./run_queries.sh $1 FLIQUE relax &
         wait
 done
